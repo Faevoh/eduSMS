0277c1a0aa3a7c60bfb2923d1a00e6d72bb067e5
unknown <favourigbani29@gmail.com>
2023-02-03 17:23:57 +0100

    Correction on Second Commit

--- Routers/adminRoute.js
+++ Routers/adminRoute.js
@@ -11,9 +11,9 @@ Route.route("/userVerify/:id").post(confirmVerify);
 Route.route("/verifyStudent/:studentid").post(confirmVerified);
 Route.route("/admin/login").post(adminLogin);
 Route.route("/student/login").post(studentLogin);
-Route.route("/admin/:userid").post(roleAuth,newStudent);
-Route.route("/admin/allStudent/:userid").get(roleAuth,getAllStudents);
-Route.route("/admin/deleteStudent/:userid/:studentid").delete(roleAuth,deleteStudents);
+Route.route("/admin/:userid",roleAuth).post(newStudent);
+Route.route("/admin/allStudent/:userid",roleAuth).get(getAllStudents);
+Route.route("/admin/deleteStudent/:userid/:studentid",roleAuth).delete(deleteStudents);
 // Route.route("/admin/image/:id").patch(updateImage);
 
 
